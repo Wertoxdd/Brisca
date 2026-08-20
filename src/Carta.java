33a17fb9735349b2a00003e13d353f0fd25aa3c9
Wertoxdd <atep9390@gmail.com>
2026-08-20 20:19:26 +0200

    Initial commit

---
+++ src/Carta.java
@@ -0,0 +1,43 @@
+public class Carta {
+    private Palo palo;
+    private int valor;
+
+    public Carta(Palo pPalo, int pValor){
+        this.palo = pPalo;
+        this.valor = pValor;
+    }
+
+    public Palo getPalo(){
+        return this.palo;
+    }
+
+    public int getValor(){
+        return this.valor;
+    }
+
+    @Override
+    public String toString(){
+
+        String val;
+
+        if (this.valor == 1){
+            val = "As" ;
+        }
+        else if (this.valor == 3){
+            val = "Tres";
+        }
+        else if (this.valor == 10){
+            val = "Sota";
+        }
+        else if (this.valor == 11){
+            val = "Caballo";
+        }
+        else if (this.valor == 12){
+            val = "Rey";
+        }
+        else{
+            val = String.valueOf(this.valor);
+        }
+        return val + " de " + this.palo;
+    }
+}
---
+++ src/Palo.java
@@ -0,0 +1,6 @@
+public enum Palo {
+    OROS,
+    BASTOS,
+    ESPADAS,
+    COPAS;
+}
